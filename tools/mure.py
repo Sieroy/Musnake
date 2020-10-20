c07cc6a8612fcde87c828cb90b406a637bc99d4b
Sieroy <Sieroy@users.noreply.github.com>
2020-10-20 22:28:53 +0800

    Update document.md

--- tools/mure.py
+++ tools/mure.py
@@ -1,7 +1,8 @@
-# 键盘监听
+from sys import argv
 from pynput.keyboard import Key,Listener
 from time import time
 
+path = '\\'.join(argv[0].split('\\')[:-1])
 timeList = []
 l = None
 
@@ -10,8 +11,8 @@ def on_press(key):
     if key != Key.esc:
         timeList.append(time()*1000)
     else:
-        f = open(r'C:\\Users\Sieroy\Desktop\666.txt', 'w')
-        f.write(','.join([str(int(i-timeList[0])) for i in timeList]))
+        f = open(path+'\\notes.txt', 'w')
+        f.write('['+','.join([str(int(i-timeList[0])) for i in timeList])+']')
         f.close()
         l.stop()
         
